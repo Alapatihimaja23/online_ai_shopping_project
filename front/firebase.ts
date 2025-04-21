f51000a3809797fa03829640136e571b778d98fe
Alapatihimaja23 <himajaalapati23@gmail.com>
2025-04-21 20:57:17 +0530

    added firebase config

---
+++ front/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAPu_bcDrDxMlAmlXxtPMFOv8nQ_3U92CE",
+  authDomain: "manashop-online.firebaseapp.com",
+  projectId: "manashop-online",
+  storageBucket: "manashop-online.firebasestorage.app",
+  messagingSenderId: "176495777351",
+  appId: "1:176495777351:web:ef411a6c0d7b3eb3934362",
+  measurementId: "G-SSDM1KEV9E"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
